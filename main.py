31a3ef5d00f85e55c4f22148e85b792f0f8361d8
MarkyMan4 <markus.hutnik@gmail.com>
2022-01-01 01:12:36 -0600

    added betting system

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ class Player:
     def __init__(self, initial_points=100):
         self._cards = [] # cards in the players hand
         self._points = initial_points # initial points available to bet
+        self._bet = 0 # initialize bet to 0, this will be set later
 
     # if the player is the dealer, the first card dealt will be face down, all other cards are face up
     # if the player is not the dealer, all cards are dealt face up
@@ -40,6 +41,18 @@ class Player:
     @property
     def points(self):
         return self._points
+
+    @points.setter
+    def points(self, points):
+        self._points = points
+
+    @property
+    def bet(self):
+        return self._bet
+
+    @bet.setter
+    def bet(self, bet):
+        self._bet = bet
 
 # handles creation of deck, players and main loop
 class Game:
@@ -80,15 +93,32 @@ class Game:
         print('    q - quit the game')
         print()
 
+    def handle_player_won_round(self):
+        print('**You won!**')
+        print(f'Your total is {self.player.get_card_total()}')
+        print(f"The dealer's total is {self.dealer.get_card_total()}")
+
+        # add double the amount of points bet since the bet has already been subtracted from points
+        self.player.points += self.player.bet * 2
+
+    def handle_player_lost_round(self):
+        print('**You lost!**')
+        print(f'Your total is {self.player.get_card_total()}')
+        print(f"The dealer's total is {self.dealer.get_card_total()}")
+
+    def handle_draw_round(self):
+        print('Draw')
+
+        # add the bet back to point total
+        self.player.points += self.player.bet
+
     # play the dealers hand and finish the round
     def end_round(self, player_busted=False):
         player_total = self.player.get_card_total()
         dealer_total = self.dealer.get_card_total()
         
         if player_busted:
-            print('**You lost!**')
-            print(f'Your total is {player_total}')
-            print(f"The dealer's total is {dealer_total}")
+            self.handle_player_lost_round()
         else:
             # play the dealers hand and show their cards and score
             self.play_dealer_hand()
@@ -101,22 +131,17 @@ class Game:
             print(f"dealer's hand total: {self.dealer.get_card_total()}")
             print()
 
-            # compare the player's total to the dealer's and determine who won, then end the round
-            player_total = self.player.get_card_total()
-            dealer_total = self.dealer.get_card_total()
             dealer_busted = dealer_total > 21
 
             if player_total > dealer_total or dealer_busted:
-                print('**You won!**')
-                print(f'Your total is {player_total}')
-                print(f"The dealer's total is {dealer_total}")
+                self.handle_player_won_round()
             elif not dealer_busted and (player_total < dealer_total or player_busted):
-                print('**You lost!**')
-                print(f'Your total is {player_total}')
-                print(f"The dealer's total is {dealer_total}")
+                self.handle_player_lost_round()
             else:
-                print('Draw')
+                self.handle_draw_round()
 
+        print()
+        print(f'** You have {self.player.points} points **')
         print()
 
         input('Press enter the start the next round\n')
@@ -159,6 +184,8 @@ class Game:
     def play(self):
         # main loop
         while True:
+            print(f'** You have {self.player.points} points **\n')
+
             # both dealer and player start with two cards
             for _ in range(2):
                 self.player.give_card(self.deck.deal_card())
@@ -166,8 +193,27 @@ class Game:
             
             self.show_dealer_hand()
             self.show_player_hand()
-
             self.round_in_progress = True
+
+            # make player enter a positive integer for the bet
+            valid_bet = False
+
+            while not valid_bet:
+                bet = input(f'You have {self.player.points} points. Enter your bet: ')
+
+                try:
+                    bet = int(bet)
+
+                    if bet > 0:
+                        self.player.bet = bet
+                        valid_bet = True
+                    else:
+                        print('Enter a positive whole number for your bet\n')
+                except:
+                    print('Enter a positive whole number for your bet\n')
+                
+            self.player.points -= self.player.bet
+            print()
             
             while self.round_in_progress:
                 command = input("What do you want to do? (type 'help' to see your options): ")
